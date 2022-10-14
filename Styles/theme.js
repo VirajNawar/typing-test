9447e874b351421674720099ccf4cdb02daba12d
Viraj <104824172+VirajNawar@users.noreply.github.com>
2022-10-14 18:56:46 +0530

    Add files via upload

---
+++ Styles/theme.js
@@ -0,0 +1,14 @@
+const darkTheme = {
+    background: "#293241",
+    color: "#7bdff2"
+}
+
+const lightTheme = {
+    background: "#7bdff2",
+    color: "#293241"
+}
+
+export const themeOptions = [
+    {value: darkTheme, label:"Dark"},
+    {value: lightTheme, label: "Light"}
+];
